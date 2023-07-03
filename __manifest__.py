fb321963ded2f910f5a7736ef99c5fb7f773f1c1
Fauzun Naja <70993035+Funazu@users.noreply.github.com>
2023-07-03 10:56:40 +0700

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "TAC Koperasi",
+    'summary': """
+     Sistem Koperasi
+        """,
+    'description': """
+        Sistem Koperasi""",
+    'author': "Teknologi Aplikasi Cerdas",
+    'company': "Teknologi Aplikasi Cerdas",
+    'maintainer': 'Teknologi Aplikasi Cerdas',
+    'website': "",
+    "license": "AGPL-3",
+    'category': 'Education',
+    'version': '15.0.1.0.0',
+    'depends': ['base'
+                ],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
